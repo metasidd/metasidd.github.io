11f035f26f2b72768679cea7e28561308ebec98e
Siddhant Mehta <siddhant92>
2019-01-16 01:53:23 -0800

    need for fix transition from post > home.  WIP

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
     var lastElementClicked;
     var PrevLink = document.querySelector('a.prev');
     var NextLink = document.querySelector('a.next');
-    var headerHeight = $("#navHeader").height();
+    var headerHeight, navHeaderHeight;
+    
+    var largeHeader = $("#headerIntroText");
+    var navHeader = $("#navHeader");
     
     Barba.Pjax.init();
     Barba.Prefetch.init();
@@ -51,34 +54,84 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("movepages start");
         var _this = this;
         var goingForward = true;
-    
+        navHeaderHeight = $(navHeader).height();
+        headerHeight = $(largeHeader).height();
+        totalHeight = navHeaderHeight;
+
         if (this.getNewPageFile() === this.oldContainer.dataset.prev) {
             goingForward = false;
         }
+
+        if (window.location.href == "/") {
+
+            console.log("showing header: " + navHeader + ", " +  largeHeader);
+            
+            totalHeight = navHeaderHeight + headerHeight;
+            //show big text header 
+            TweenLite.set(largeHeader, {
+                visibility: 'visible',
+                position: `relative`,
+                opacity: 0,
+                onComplete: function() {
+            }});
+            TweenLite.to(largeHeader, 0.5, {
+                position: `relative`,
+                opacity: 100,
+                // y: navHeaderHeight,
+                onComplete: function() {
+                    console.log("show complete large header to position");
+                    TweenLite.set(largeHeader, { clearProps: 'all' });
+                }
+            });
+        }
+        else{
+            totalHeight = navHeaderHeight;
+            console.log("hiding header: " + navHeader + ", " +  largeHeader);
+            //hide big text header 
+            TweenLite.set(largeHeader, {
+                visibility: 'visible',
+                position: `absolute`
+                // top: _this.y
+            });
+            TweenLite.to(largeHeader, 0.5, {
+                position: `absolute`,
+                // height: 0,
+                opacity: 0,
+                // top: (_this.y - 15),
+                onComplete: function() {
+                    console.log("hiding large header to position");
+                    TweenLite.set(largeHeader, {  });
+                // _this.done();
+            }});
+
+            //switch between screens
+            TweenLite.set(this.newContainer, {
+                visibility: 'visible',
+                position: `absolute`,
+                y: goingForward ? (totalHeight + 32) : -100,
+                opacity: 0,
+                left: 0,
+                top: 0
+                // z: 9998
+            });
+        
+            TweenLite.to(this.newContainer, 0.5, {
+                delay: 0.13,
+                y: totalHeight,
+                top: 0,
+                opacity: 100,
+                onComplete: function() {
+                TweenLite.set(_this.newContainer, { clearProps: 'all' });
+                _this.done();
+            }});
     
-        TweenLite.set(this.newContainer, {
-            visibility: 'visible',
-            position: `fixed`,
-            xPercent: goingForward ? 5 : -100,
-            opacity: 0,
-            left: 0,
-            top: headerHeight
-            // z: 9998
-        });
+            TweenLite.to(this.oldContainer, 0.3, { 
+                y: goingForward ? -32 : 100,
+                opacity: 0
+                // z: 2
+            });
+        }
     
-        TweenLite.to(this.oldContainer, 0.3, { 
-            xPercent: goingForward ? -5 : 100,
-            opacity: 0
-            // z: 2
-        });
-        TweenLite.to(this.newContainer, 0.5, {
-            delay: 0.3,
-            xPercent: 0,
-            opacity: 100,
-            onComplete: function() {
-            TweenLite.set(_this.newContainer, { clearProps: 'all' });
-            _this.done();
-        }});
         },
     
         getNewPageFile: function() {
